ee35e02cf24089b4d2992dc2da8fc1f37227890a
Sherwin Guanlao <sherwin.guanlao@gmail.com>
2024-05-17 17:08:37 +0800

    installed laravel and jetstream/vue

---
+++ resources/js/Components/AuthenticationCardLogo.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+</script>
+
+<template>
+    <Link :href="'/'">
+        <ApplicationLogo class="w-full" />
+    </Link>
+</template>
