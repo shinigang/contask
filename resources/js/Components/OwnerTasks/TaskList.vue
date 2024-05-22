6dd75757a6253b7f141cb82b4607ef63a599997e
Sherwin Guanlao <sherwin.guanlao@gmail.com>
2024-05-23 02:52:08 +0800

    fix: dark mode texts

--- resources/js/Components/OwnerTasks/TaskList.vue
+++ resources/js/Components/OwnerTasks/TaskList.vue
@@ -9,13 +9,13 @@ defineProps({
         <li v-for="task in tasks" class="flex justify-between gap-x-6 py-5">
             <div class="flex min-w-0 gap-x-4">
                 <div class="min-w-0 flex-auto">
-                    <p class="text-sm font-semibold leading-6 text-gray-900">{{ task.name }}</p>
-                    <p class="mt-1 text-xs leading-5 text-gray-500 line-clamp-3">{{ task.description }}</p>
+                    <p class="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-100">{{ task.name }}</p>
+                    <p class="mt-1 text-xs leading-5 text-gray-500 dark:text-gray-400 line-clamp-3">{{ task.description }}</p>
                 </div>
             </div>
             <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
                 <span class="capitalize inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">{{ task.status }}</span>
-                <p class="mt-1 text-xs leading-5 text-gray-500">Assigned <time :datetime="task.created_at">{{ task.created_time_ago }}</time></p>
+                <p class="mt-1 text-xs leading-5 text-gray-500 dark:text-gray-300">Assigned <time :datetime="task.created_at">{{ task.created_time_ago }}</time></p>
             </div>
         </li>
     </ul>
